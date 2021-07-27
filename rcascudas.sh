ed6e60a1efd8e5b28f98eb40a1edadce2ecf6afa
Ran-n <ran-n@tutanota.com>
2021-07-27 13:42:39 +0200

    sh execución continuada cascudas

---
+++ rcascudas.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#+ Autor:	Ran#
+#+ Creado:	02/04/2021 18:40:32
+#+ Editado:	27/07/2021 13:42:17
+
+REP=1
+
+execucion() {
+    echo ''
+    echo '> Repetición #'$REP
+    ./cascudas.py -d $dataset -e $epoch -b $batch
+    REP=$((REP+1))
+}
+
+for dataset in 32 64 128 256; do
+     for epoch in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 20 25 30 35 40 45 50; do
+        for batch in 8 16 32 64 128 256; do
+            execucion
+            execucion
+            execucion
+        done
+    done
+done
