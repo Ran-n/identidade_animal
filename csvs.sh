25a1cd3ae178aa39868f8f2539cdb7426ff58a5b
Ran-n <ran-n@tutanota.com>
2021-07-31 10:35:40 +0200

    autocreación da carpeta

--- csvs.sh
+++ csvs.sh
@@ -3,9 +3,9 @@
 #+ Creado:	31/07/2021 10:12:27
 #+ Editado:	31/07/2021 10:20:12
 
-#[[ -n "${2+set}" ]] && ./mover_metricas.sh
+#[[ -n "${2+set}" ]] && ./mover_metricas.sh metricas
 
 if [ -n "${2+set}" ]; then
-    ./mover_metricas.sh
+    ./mover_metricas.sh metricas
 fi
 ./ms2csv.py -d $1
--- mover_metricas.sh
+++ mover_metricas.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 #+ Autor:	Ran#
 #+ Creado:	31/07/2021 00:15:56
-#+ Editado:	31/07/2021 00:15:56
+#+ Editado:	31/07/2021 10:22:59
 
-for ele in $(ls saidas/*/*/*.metricas); do cp $ele metricas; done
+mkdir -p $1
+for ele in $(ls saidas/*/*/*.metricas); do cp $ele $1; done
